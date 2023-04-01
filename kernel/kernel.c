f346ba6cab33977b80da496c8f4580446ebf25dc
Adam Wright <adam.t.wright@outlook.com>
2023-04-01 16:29:40 +0100

    add port functions and update welcome message

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,17 @@
 void main() {
   char* video_memory = (char *) 0xb8000;
 
-  *video_memory = 'X';
+  video_memory[0] = 'H';
+  video_memory[2] = 'e';
+  video_memory[4] = 'l';
+  video_memory[6] = 'l';
+  video_memory[8] = 'o';
+  video_memory[10] = ',';
+  video_memory[12] = ' ';
+  video_memory[14] = 'W';
+  video_memory[16] = 'o';
+  video_memory[18] = 'r';
+  video_memory[20] = 'l';
+  video_memory[22] = 'd';
+  video_memory[24] = '!';
 }
