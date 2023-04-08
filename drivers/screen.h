6a331bd8a24bf055a39c470b291374dd071a44ce
Adam Wright <adam.t.wright@outlook.com>
2023-04-08 01:12:16 +0100

    remove screen define directives from header

--- drivers/screen.h
+++ drivers/screen.h
@@ -1,15 +1,6 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#define VIDEO_MEMORY_START_ADDRESS 0xb8000
-#define MAXIMUM_ROWS 25
-#define MAXIMUM_COLUMNS 80
-#define WHITE_ON_BLACK 0x0f
-#define SCREEN_CONTROL_REGISTER 0x3d4
-#define SCREEN_DATA_REGISTER 0x3d5
-#define CURSOR_LOCATION_HIGH_REGISTER 14
-#define CURSOR_LOCATION_LOW_REGISTER 15
-
 int get_screen_offset(int column, int row);
 
 int get_cursor();
