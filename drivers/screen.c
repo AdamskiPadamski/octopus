a81ec49e8c79fe71bdf02b2bbce9402ef6674bf3
Adam Wright <adam.t.wright@outlook.com>
2023-04-08 01:01:20 +0100

    move screen define directives to implementation

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,6 +1,15 @@
 #include "../kernel/low_level.h"
 #include "./screen.h"
 
+#define VIDEO_MEMORY_START_ADDRESS 0xb8000
+#define MAXIMUM_ROWS 25
+#define MAXIMUM_COLUMNS 80
+#define WHITE_ON_BLACK 0x0f
+#define SCREEN_CONTROL_REGISTER 0x3d4
+#define SCREEN_DATA_REGISTER 0x3d5
+#define CURSOR_LOCATION_HIGH_REGISTER 14
+#define CURSOR_LOCATION_LOW_REGISTER 15
+
 int get_screen_offset(int column, int row) {
   return (row * MAXIMUM_COLUMNS + column) * 2;
 }
